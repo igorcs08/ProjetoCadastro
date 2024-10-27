e603dfb2e701e58788d24e231cd9c299da0854ee
Igor Cavalcante <igor.cavalcante96@gmail.com>
2024-10-27 12:17:36 -0300

    Ajusta Forms, Controller e Model adicionando o campo CPF. Ajusta jQuery para buscar e enviar o dado do campo CPF.

--- FI.WebAtividadeEntrevista/Models/ClienteModel.cs
+++ FI.WebAtividadeEntrevista/Models/ClienteModel.cs
@@ -67,5 +67,9 @@ namespace WebAtividadeEntrevista.Models
         /// </summary>
         public string Telefone { get; set; }
 
+        /// <summary>
+        /// CPF
+        /// </summary>
+        public string CPF { get; set; }
     }    
 }
